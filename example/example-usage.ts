10b270aa9d868263271b01bfcc098f2cca95eb6f
Maciej Kulon <kaczal1992@gmail.com>
2022-11-19 16:56:14 +0100

    chore(): update example usage

--- example/example-usage.ts
+++ example/example-usage.ts
@@ -1,8 +1,11 @@
 import { MediaInfo } from '../src';
-
+ 
 async function exec() {
   const mediaInfo = new MediaInfo();
-  mediaInfo.getMediaInfoData('https://example-segment.url/0001.ts');
+  await mediaInfo.instantiateLib();
+  const data = await mediaInfo.getInfo('http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4');
+  console.log(JSON.stringify(data, null, 2));
 }
-
+ 
+ 
 exec();
