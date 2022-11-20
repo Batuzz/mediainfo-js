722e24466002280c119f953bbd6b74095be721fb
Maciej Kulon <kaczal1992@gmail.com>
2022-11-20 17:35:04 +0100

    feat(): add local file stream

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
 import * as MediaInfoLib from '../lib/MediaInfoWasm';
+import * as fs from 'fs';
 import got from 'got';
+import { MediaInfoError, MediaInfoInputError } from './errors';
  
 export type MediaInfoInput = URL | string;
  
 export type MediaInfoData = any; // maybe we'll add concrete types in future
  
 type ErrorHandlerFunction = (reason: any) => void;
- 
-export class MediaInfoError extends Error {
-  constructor(public readonly message: string, public readonly higherOrderError: any) {
-    super(message);
-    Object.setPrototypeOf(this, new.target.prototype);
-  }
-}
- 
+
 export class MediaInfo {
   private lib;
  
@@ -51,16 +46,45 @@ export class MediaInfo {
  
     return normalizedInput;
   }
+
+  private static openFileStream(input: string) {
+    try {
+      const stream = fs.createReadStream(input);
+      if(stream.errored) {
+        throw stream.errored;
+      }
+      return stream;
+    } catch (err) {
+      throw new MediaInfoInputError('Could not open read stream to the file.', input, err);
+    }
+  }
+
+  private static openHttpStream(input: string) {
+    try {
+      const stream = got.stream(input);
+      if(stream.errored) {
+        throw stream.errored;
+      }
+      return stream;
+    } catch (err) {
+      throw new MediaInfoInputError('Could not open read stream from HTTP address.', input, err);
+    }
+  }
  
   private static getDataStream(input: string, errorHandler: ErrorHandlerFunction) {
     let stream;
-
     if (input.toLowerCase().startsWith('http')) {
-      stream = got.stream(input);
+      stream = this.openHttpStream(input);
+    } else if (fs.existsSync(input)) {
+      stream = this.openFileStream(input);
     } else {
-      throw new Error('Input other than HTTP is not supported yet');
+      throw new MediaInfoInputError('Input is neither HTTP address nor file path.', input);
     }
- 
+    
+    if(!stream) {
+      errorHandler(`Could not create any stream for a given input: ${input}`);
+    }
+    
     stream.on('error', errorHandler);
  
     return stream;
@@ -72,11 +96,9 @@ export class MediaInfo {
  
       mediaInfoInstance.Open_Buffer_Init(-1, 0);
  
-      let seekTo: number;
- 
       stream.on('data', (chunk) => {
         mediaInfoInstance.Open_Buffer_Continue(chunk);
-        seekTo = mediaInfoInstance.Open_Buffer_Continue_Goto_Get();
+        mediaInfoInstance.Open_Buffer_Continue_Goto_Get();
       });
  
       stream.on('end', () => {
